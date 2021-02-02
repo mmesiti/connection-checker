16a61d7b42cb3d51ce276f4e327de8dd62aee492
Michele Mesiti <michele.mesiti@gmail.com>
2021-02-02 15:03:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Readme
+
+Usage:
+```
+git clone https://github.com/mmesiti/connection-checker
+./connection-checker/check_connection.sh&
+```
+You will keep getting data in a csv file 
+in the `connection-checker` directory, that you can analyse later.
+
---
+++ check_connection.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+get_source_dir(){
+    ( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )
+}
+
+
+CONNECTION_DATA=$(get_source_dir)/connection_data.txt
+
+ping_google(){
+ping -W 2 -c 1 8.8.8.8 | grep loss 
+}
+
+echo Saving data in $CONNECTION_DATA.
+
+if [ ! -f "$CONNECTION_DATA" ]
+then
+    echo "Date ; UnixTime; PingMessage" >> $CONNECTION_DATA
+fi
+
+while true
+do
+    echo $(date) \; $(date +"%s") \; $(ping_google) >> $CONNECTION_DATA
+    sleep 5
+done
